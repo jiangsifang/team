b08f522f46db17b9bb48f6bd9339f6a438820cdc
jiangsifang <691887420@qq.com>
2020-11-25 08:58:09 +0800

    第一次提交

---
+++ Team/src/com/nnxy/team1/Jiangsifnag.java
@@ -0,0 +1,6 @@
+package com.nnxy.team1;
+
+public class Jiangsifnag {
+	private String name;
+	private String author;
+}
---
+++ Team/src/com/nnxy/team2/Jiangsifang.java
@@ -0,0 +1,8 @@
+package com.nnxy.team2;
+
+public class Jiangsifang {
+
+	private String name;
+	private String author;
+	
+}
